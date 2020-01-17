f2cb05048b2f00dd63427c634a7996c2d6c0f22e
Roman Romashko <rr@listex.info>
2020-01-17 13:00:32 +0200

    Math

---
+++ Math_calc.py
@@ -0,0 +1,19 @@
+import math
+
+
+print("Hi. This is simple calculation software.")
+x = int(input("Please, indicate your number : "))
+y = int(input("""Please specify what do you want to do with the number : ")
+    1. Calculate sin
+    2. Calculate cos
+    3. Calculate log
+    4. Calculate the square
+    : """))
+if y == 1:
+    print("sin x = ", math.sin(x))
+elif y == 2:
+    print("cos x = ", math.cos(x))
+elif y == 3:
+    print("log x = ", math.log(x))
+elif y == 4:
+    print("x ^ 2 = ", math.pow(x,2))

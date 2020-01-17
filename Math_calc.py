336bd888c526a9b4dfb54deac8f27a070a6a4283
Roman Romashko <rr@listex.info>
2020-01-17 15:47:19 +0200

    Some universalizing

--- Math_calc.py
+++ Math_calc.py
@@ -3,12 +3,24 @@ import math
 
 print("Hi. This is simple calculation software.")
 x = float(input("Please, indicate your number : "))
+#Как проверять тип данных, вводимый пользователем и перезапрашивать в случая введения некорректных ?
+#print(bool(str(x)))
+#while type(float(x)) != int:# or type(x) != float or type(x) != complex:
+#    x = print(input("You've entered text. Please, enter numeric value :"))
+
 y = int(input("""Please specify what do you want to do with the number :
     1. Calculate sin
     2. Calculate cos
     3. Calculate log
     4. Calculate the square
     : """))
+while y > 4 or y <= 1:
+    y = int(input("""Incorrect value. Please specify what do you want to do with the number :
+    1. Calculate sin
+    2. Calculate cos
+    3. Calculate log
+    4. Calculate the square
+    :"""))
 if y == 1:
     print("sin x = ", math.sin(x))
 elif y == 2:
